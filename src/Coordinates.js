83df9fc16e8e42ea72624958113c046ef576de6e
mvoronin <voronin.mark123@gmail.com>
2017-10-02 13:43:16 +0200

    Histoire 2 commit code (test vrai car 43 coordonnées valides)

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -5,8 +5,19 @@ Lyngk.Coordinates = function (c, l) {
     var ligne=l;
 
     this.isOk=function(){
-        if(colonne=='A' && ligne==1){
-            return false;
+        var tabOk= {
+                "A" : [3, 3],
+                "B" : [2, 5],
+                "C" : [1, 7],
+                "D" : [2, 7],
+                "E" : [2, 8],
+                "F" : [3, 8],
+                "G" : [3, 9],
+                "H" : [5, 8],
+                "I" : [7, 7]
+            };
+        if(l>=tabOk[c][0] && l<=tabOk[c][1]){
+            return true;
         }
     }
 };
