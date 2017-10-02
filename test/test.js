2503c1018156e57c6b1c91c07a36471c9e9bfd80
mvoronin <voronin.mark123@gmail.com>
2017-10-02 13:25:02 +0200

    Histoire 2 commit test

--- test/test.js
+++ test/test.js
@@ -4,5 +4,13 @@ var LyngkTestCase1 = TestCase("LyngkTestCase");
 
 LyngkTestCase1.prototype.testA=function(){
     var c=new Lyngk.Coordinates('A',1);
-    assertFalse(c.isOk());
+    var cpt=0;
+    var lettres='ABCDEFGHI';
+    for(var i=1;i<=9;i++){
+        for(var j=1;j<=9;j++){
+            if(assertTrue(c.isOk(lettres[i],j))){
+                cpt++;
+            }
+        }
+    }
 };
