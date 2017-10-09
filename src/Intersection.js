fbffc55984c0bca5e4cf3215f797065f3dbe4fe7
mvoronin <voronin.mark123@gmail.com>
2017-10-09 11:18:12 +0200

    Histoire 7 test success

--- src/Intersection.js
+++ src/Intersection.js
@@ -3,4 +3,10 @@
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
 Lyngk.Intersection = function (c) {
+    var coor=c;
+    var etat=Lyngk.State.VACANT;
+
+    this.getEtat=function(){
+        return etat;
+    }
 };
